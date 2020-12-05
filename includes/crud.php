1eb20480d449b34f54d5e5acd120b0ca85bf7233
kamlesh sahu <vasubantikamlesh@gmail.com>
2020-12-05 08:35:17 -0800

    Update crud.php

--- includes/crud.php
+++ includes/crud.php
@@ -19,9 +19,9 @@ class Database
      */
 
     private $db_host = "localhost";  // Change as required
-    private $db_user = "root";  // Change as required
-    private $db_pass = "";  // Change as required
-    private $db_name = "ecart";    // Change as required
+    private $db_user = "hsgjbsmzsv";  // Change as required
+    private $db_pass = "#Vasubanti026K";  // Change as required
+    private $db_name = "hsgjbsmzsv";    // Change as required
 
     /*
      * Extra variables that are required by other function such as boolean con variable
